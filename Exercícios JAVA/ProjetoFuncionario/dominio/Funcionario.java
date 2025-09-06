d4c5dace0f55655b9f79f2156be57a521f1ac8bf
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-09-05 21:50:51 -0300

    Add files via upload

---
+++ Exercícios JAVA/ProjetoFuncionario/dominio/Funcionario.java
@@ -0,0 +1,138 @@
+package ProjetoFuncionario.dominio;
+
+// >> CLASSE Funcionario
+public class Funcionario {
+    private String nome;
+    private char sexo;
+    private String cpf;
+    private double salarioBruto;
+
+// >> CONSTRUTORES
+    public Funcionario(){
+        this.nome = "";
+        this.sexo = ' ';
+        this.cpf = "";
+        this.salarioBruto = 0;
+    }
+
+    // Estava assim, sem o SET 
+    // public Funcionario(String n, char s, String cpf, double sal){
+    //     this.nome = n;
+    //     this.sexo = s;
+    //     this.cpf = cpf;
+    //     this.salarioBruto = sal;
+    // }
+
+    // Mas o CORRETO é colocar o SET dessa forma... pq caso houvesse uma validação na função  SET, ele seria chamado para testar e só assim permitiria a construção da classe.
+    public Funcionario(String n, char s, String cpf, double sal){
+        this.setNome(n);
+        this.setSexo(s);
+        this.setCPF(cpf);
+        this.setSalarioBruto(sal);
+    }
+
+// >> MÉTODOS
+    // Taxa do INSS - Faço a verificação de cada condicional
+    public double txINSS(){
+        if (salarioBruto < 0){
+            throw new RuntimeException("Erro: Insira um salário bruto positivo.");
+        } else if(salarioBruto <= 1000.00){
+            return 0.08;
+        } else if(salarioBruto <= 2000.00){
+            return 0.09;
+        } else if(salarioBruto <= 3000.00){
+            return 0.10;
+        } else {
+            return 0.11;
+        }
+    }
+
+    // Valor do INSS - Calculo o valor do INSS a ser pago
+    public double valorINSS(){
+        double tetoINSS = 900.00; // Defini o teto
+        double aliquota = txINSS(); // Armazenei a taxa em uma variável
+
+        double valorCalculado = salarioBruto * aliquota;
+
+        // Verifico se entra no caso do teto máximo ou não
+        if(valorCalculado >= tetoINSS){
+            return tetoINSS;
+        } else {
+            return valorCalculado;
+        }
+    }
+
+    // Calcula o valor do salario liquido
+    public double salarioLiquido(){
+        double liquido = salarioBruto - valorINSS() - valorIR();
+        return liquido;
+    }
+
+     // Salário Base IR
+     public double salarioBaseIR(){
+        double salarioBase = salarioBruto - valorINSS();
+        return salarioBase;
+    }
+
+    // Taxa do Imposto de Renda
+    public double txIR(){
+        if (salarioBaseIR() <= 1000.00){
+            return 0.00;
+        } else if(salarioBaseIR() > 1000.00 && salarioBaseIR() <= 3000.00){ // CORREÇÃO: Usado '&&'
+            return 0.10;
+        } else if(salarioBaseIR() > 3000.00 && salarioBaseIR() <= 5000.00){ // CORREÇÃO: Usado '&&'
+            return 0.20;
+        } else if(salarioBaseIR() > 5000.00){
+            return 0.30;
+        } else {
+            throw new RuntimeException("Erro na verificação da taxa de IR.");
+        }
+    }
+
+    // Valor do Imposto de Renda
+    public double valorIR(){
+        double valorImpostoRenda = salarioBaseIR() * txIR();
+        return valorImpostoRenda;
+    }
+
+// >> SETTERS E GETTERS
+    // SET nome
+    public void setNome(String n){
+        this.nome = n;
+    }
+    // GET nome
+    public String getNome(){
+        return nome;
+    }
+
+
+    // SET sexo
+    public void setSexo(char s){
+        this.sexo = s;
+    }
+    // GET sexo
+    public char getSexo(){
+        return sexo;
+    }
+
+
+    // SET cpf
+    public void setCPF(String cpf){
+        this.cpf = cpf;
+    }
+    // GET cpf
+    public String getCPF(){
+        return cpf;
+    }
+
+
+    // SET salarioBruto
+    public void setSalarioBruto(double s){
+        this.salarioBruto = s; 
+    }
+    // GET salarioBruto
+    public double getSalarioBruto(){
+        return salarioBruto;
+    }
+        
+}
---
+++ Exercícios JAVA/ProjetoFuncionario/testes/TestaFuncionario.java
@@ -0,0 +1,26 @@
+package ProjetoFuncionario.testes;
+import ProjetoFuncionario.dominio.Funcionario;
+
+public class TestaFuncionario {
+    public static void main(String[] args) {
+        Funcionario f1;
+        f1 = new Funcionario("João Silva", 'M', "123.456.789-99", 10000.00 );
+        // Para testar se funciona, apenas alterar os valores acima :)
+
+        System.out.println("--------------------------------------");
+        System.out.println("Dados do Funcionario");
+        System.out.println("--------------------------------------");
+        System.out.println("Nome: " + f1.getNome());
+        System.out.println("CPF: " + f1.getCPF());
+        System.out.println("Sexo: " + f1.getSexo());
+        System.out.println(".................................");
+        System.out.println("Salario Bruto: R$ " + f1.getSalarioBruto());
+        System.out.println("Taxa do INSS: " + f1.txINSS());
+        System.out.println("Valor INSS: R$ " + f1.valorINSS());
+        System.out.println("--------------------------------------");
+        System.out.println("Salario Base IR: R$ " + f1.salarioBaseIR());
+        System.out.println("IR: " + f1.txIR());
+        System.out.println("--------------------------------------");
+        System.out.println("Salario Liquido: R$ " + f1.salarioLiquido());
+    }
+}
