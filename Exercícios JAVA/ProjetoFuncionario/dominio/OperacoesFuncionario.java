5cda8025301b62e6718ad5451169a642dd8e13ab
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-09-11 23:59:11 -0300

    Update OperacoesFuncionario.java

--- Exercícios JAVA/ProjetoFuncionario/dominio/OperacoesFuncionario.java
+++ Exercícios JAVA/ProjetoFuncionario/dominio/OperacoesFuncionario.java
@@ -1,13 +1,10 @@
 import ProjetoFuncionario.dominio.Funcionario;
 
 public class OperacoesFuncionario {
-
 // MÉTODOS
     // AUMENTO DE SALÁRIO 
-
     public static void aumentaSalario(Funcionario f, double porcentagem){
     // verificações basicas de ZERO e NULL
-
         if(func == null){
             throw new RuntimeException("Erro: Insira um funcionário válido.");
         }
@@ -15,22 +12,19 @@ public class OperacoesFuncionario {
         if(porcentagem < 0){
             throw new RuntimeException("A porcentagem deve ser maior que zero!");
         }
-
+        
         double salarioAtual = func.getSalarioBruto(); // pegando a info necessária
         double calculoAumento = salarioAtual * (porcentagem / 100.00); // calculo o aumento
         double novoSalario = salarioAtual + calculoAumento; // add ao salario atual
         f.setSalarioBruto(novoSalario); // uso o método para setar o novo salario
-
     }
 
     // TROCA DE SALÁRIO
-
     public static void trocaSalario(Funcionario f1, Funcionario f2){
-
         if(f1 == null || f2 == null){
             throw new RuntimeException("Erro: Insira um funcionário válido.");
         }
-
+        
         // armazeno o salario de f1, para nao perde-lo
         double salarioTempF1 = f1.getSalarioBruto();
 
